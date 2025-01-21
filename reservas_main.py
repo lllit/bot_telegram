89f7bd7170576d95513137b7993829160a0016a1
Matias <lit.io30303@gmail.com>
2025-01-21 16:13:16 -0300

    fix name

--- reservas_main.py
+++ reservas_main.py
@@ -349,4 +349,5 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-    flask_app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    flask_app.run(host='0.0.0.0', port=port)
